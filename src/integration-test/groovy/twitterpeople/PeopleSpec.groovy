f23fd961a69b60c87d980ca9971089fd9c8f1481
Jorge Aguilera <jagedn@gmail.com>
2016-02-18 20:17:31 +0100

    some tests

--- src/integration-test/groovy/twitterpeople/PeopleSpec.groovy
+++ src/integration-test/groovy/twitterpeople/PeopleSpec.groovy
@@ -1,17 +1,14 @@
 package twitterpeople
 
 import com.jayway.restassured.builder.RequestSpecBuilder
-import grails.plugins.rest.client.RestBuilder
-import grails.plugins.rest.client.RestResponse
 import grails.test.mixin.integration.Integration
-import grails.transaction.Rollback
 import org.junit.ClassRule
-import org.junit.Rule
 import org.springframework.restdocs.JUnitRestDocumentation
 import spock.lang.Shared
 import spock.lang.Specification
 
 import static com.jayway.restassured.RestAssured.given
+import static org.hamcrest.CoreMatchers.equalTo
 import static org.hamcrest.CoreMatchers.is
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest
 import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.document
@@ -56,6 +53,32 @@ class PeopleSpec extends Specification {
         then:
         then.assertThat().statusCode(is(200));
 
-
     }
+
+    void "test person #username"() {
+
+        given:
+        def request = given(documentationSpec)
+                .accept("application/json")
+                .filter(document("people/${document}",
+                preprocessRequest(modifyUris()
+                        .scheme("http")
+                        .host("localhost")
+                        .removePort())))
+        when:
+        def then = request
+                .when()
+                .port(8080)
+                .get("/people/${username}")
+                .then()
+
+        then:
+        then.assertThat().statusCode(is(200));
+        then.assertThat().content('id', equalTo(username));
+
+        where:
+        username | document
+        'jagedn' | "person1"
+    }
+
 }
